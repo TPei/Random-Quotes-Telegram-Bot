49cba13ff0af905a7452a35fca70ed7e0b9664bf
tpei <t.s.peikert@gmail.com>
2016-12-11 09:34:57 +0100

    add /image to get a random image, add image folder

--- main.rb
+++ main.rb
@@ -12,12 +12,18 @@ Telegram::Bot::Client.run(token) do |bot|
       responses = []
       chat_id = message.chat.id
 
-      if message.text.start_with? '/'
-        responses.push CommandWatcher.parse(message.text)
-      end
+      if message.text.strip == '/image'
+        image_path = "./images/*"
+        images = Dir[image_path]
+        bot.api.send_photo(chat_id: chat_id, photo: Faraday::UploadIO.new(images.sample, 'image/jpeg'))
+      else
+        if message.text.start_with? '/'
+          responses.push CommandWatcher.parse(message.text)
+        end
 
-      mdp = MessageDispatcher.new(bot: bot, chat_id: chat_id)
-      mdp.dispatch_batch(responses)
+        mdp = MessageDispatcher.new(bot: bot, chat_id: chat_id)
+        mdp.dispatch_batch(responses)
+      end
     end
   end
 end
