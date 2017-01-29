1cb1793d30a203fa80739fe9725683b1ec6dc605
tpei <t.s.peikert@gmail.com>
2017-01-29 14:24:07 +0100

    restart bot if api errors

--- main.rb
+++ main.rb
@@ -6,22 +6,28 @@ Dotenv.load
 
 token = ENV['BOT_TOKEN']
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    if message.text
-      dispatcher = MessageDispatcher.new(bot: bot, chat_id: message.chat.id)
+while(true)
+  begin
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        if message.text
+          dispatcher = MessageDispatcher.new(bot: bot, chat_id: message.chat.id)
 
-      if message.text.start_with? '/image'
-        image_path = "./images/*"
-        images = Dir[image_path]
-        dispatcher.send(photo: images.sample)
-      elsif message.text.start_with? '/gif'
-        image_path = "./gifs/*"
-        gifs = Dir[image_path]
-        dispatcher.send(gif: gifs.sample)
-      elsif message.text.start_with? '/'
-        dispatcher.send(text: CommandWatcher.parse(message.text))
+          if message.text.start_with? '/image'
+            image_path = "./images/*"
+            images = Dir[image_path]
+            dispatcher.send(photo: images.sample)
+          elsif message.text.start_with? '/gif'
+            image_path = "./gifs/*"
+            gifs = Dir[image_path]
+            dispatcher.send(gif: gifs.sample)
+          elsif message.text.start_with? '/'
+            dispatcher.send(text: CommandWatcher.parse(message.text))
+          end
+        end
       end
     end
+  rescue => e
+    puts e.inspect
   end
 end
